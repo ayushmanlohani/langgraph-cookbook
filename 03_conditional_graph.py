660d9401b097e0e5f1be6f5b4a1aee2b65b243a0
Ayushman Lohani <aayushmanlohani@gmail.com>
2026-07-09 23:22:00 +0530

    second advancement

--- 03_conditional_graph.py
+++ 03_conditional_graph.py
@@ -8,3 +8,24 @@ class OrderProcessing(TypedDict):
     order_id: str
     log: list[str]
     status: str
+
+
+def first_node(state: OrderProcessing):
+    print("We are in the fist node")
+    return {
+        "log": state["log"] + [" Order Received"]
+    }
+
+
+def second_node(state: OrderProcessing):
+    print("-> Confirming order")
+    return {}
+# okay so the most importnat thing here is "ROUTER FUNCTION"
+# its main purpose is to decide the next node
+
+
+def router(state: OrderProcessing) -> str:
+    if state['order_id'].startswith("ORD"):
+        return "node_b"
+    else:
+        return "node_reject"
