58188d24767c26d59319d7a574e76a44a7c23605
Ayushman Lohani <aayushmanlohani@gmail.com>
2026-07-16 02:23:06 +0530

    made the full thing

--- 09_second_agent.py
+++ 09_second_agent.py
@@ -10,13 +10,12 @@ from langgraph.prebuilt import tools_condition
 from langchain_core.messages import HumanMessage
 from datetime import datetime
 
+
 load_dotenv()
 
 
 class AgentState(TypedDict):
-    messages: Annotated[list, operator.add]
-    tool_to_use: str
-    result: str
+    messages: Annotated[list, add_messages]
 
 
 @tool
@@ -42,3 +41,54 @@ def counter(sentence: str):
         "word": word,
         "characters": characters
     }
+
+
+@tool
+def live_time(time_question: str):
+    """Tells live time."""
+    current = datetime.now()
+    return str(current)
+
+
+def router(state: AgentState):
+    last_message = state["messages"][-1]
+
+    if last_message.tool_calls:
+        return "tools"
+
+    return END
+
+
+llm = ChatGroq(model="openai/gpt-oss-20b")
+llm_with_tools = llm.bind_tools([calculator, counter, live_time])
+
+
+def llm_node(state: AgentState):
+    response = llm_with_tools.invoke(state["messages"])
+    return {
+        "messages": [response]
+    }
+
+
+tool_node = ToolNode([calculator, counter, live_time])
+
+workflow = StateGraph(AgentState)
+
+workflow.add_node("llm_node", llm_node)
+workflow.add_node("tool_node", tool_node)
+
+workflow.add_edge(START, "llm_node")
+workflow.add_conditional_edges(
+    "llm_node",
+    router,
+    {
+        "tools": "tool_node",
+        END: END
+    }
+)
+workflow.add_edge("tool_node", "llm_node")
+
+app = workflow.compile()
+result = app.invoke({"messages": [HumanMessage(
+    "What is 24 * 4?, and what is current time, how many words and cahracters are in this sentence")]})
+print(result["messages"][-1].content)
